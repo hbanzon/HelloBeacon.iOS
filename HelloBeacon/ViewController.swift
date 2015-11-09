7d09759fb642a50c0d0902fc3ab5f04100af434b
hbanzon <hino.banzon@gmail.com>
2015-11-08 23:33:51 -0500

    finished hello beacon tutorial

--- HelloBeacon/ViewController.swift
+++ HelloBeacon/ViewController.swift
@@ -8,11 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ESTBeaconManagerDelegate {
+    
+    let beaconManager = ESTBeaconManager()
+    
+    let beaconRegion = CLBeaconRegion(
+        proximityUUID: NSUUID(UUIDString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!,
+        identifier: "Entire House"
+    )
+    
+    @IBOutlet weak var nearestBeaconLabel: UILabel!
+    
+    // MARK: Sample Data Structure
+    let placesByBeacons = [
+        "31194:58554": [
+            "Heavenly Sandwiches": 50,
+            "Green & Green Salads": 150,
+            "Mini Panini": 325
+        ],
+        "63029:44225": [
+            "Heavenly Sandwiches": 250,
+            "Green & Green Salads": 100,
+            "Mini Panini": 20
+        ],
+        "52066:51215": [
+            "Heavenly Sandwiches": 350,
+            "Green & Green Salads": 500,
+            "Mini Panini": 170
+        ]
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.beaconManager.delegate = self
+        self.beaconManager.requestAlwaysAuthorization()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.beaconManager.startRangingBeaconsInRegion(beaconRegion)
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.beaconManager.stopRangingBeaconsInRegion(beaconRegion)
+    }
+    
+    //
+    // get a list of places near the specified beacon
+    //
+    func placesNearBeacon(beacon: CLBeacon) -> [String] {
+        let beaconKey = "\(beacon.major):\(beacon.minor)"
+        if let places = self.placesByBeacons[beaconKey] {
+            let sortedPlaces = Array(places).sort {$0.1 < $1.1}.map {$0.0}
+            return sortedPlaces
+        }
+        return []
+    }
+    
+    func beaconManager(manager: AnyObject, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        let nearestBeacon = beacons[0] as CLBeacon
+        let places = placesNearBeacon(nearestBeacon)
+        self.nearestBeaconLabel.text = places[0]
+        print(places)
+        
     }
 
     override func didReceiveMemoryWarning() {
